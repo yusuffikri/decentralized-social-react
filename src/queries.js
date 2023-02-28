631ae8162ee5c26d6a5e499893a5d7deb99fc99e
yusuffikri <yusuffiks99@gmail.com>
2023-02-28 14:52:12 +0800

    project setup

---
+++ src/queries.js
@@ -0,0 +1,92 @@
+import { createClient } from "urql";
+
+export const APIURL ="https://api.lens.dev";
+export const LENS_HUB_CONTRACT_ADRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
+
+export const urlClient = createClient({
+    url: APIURL,
+});
+
+export const queryRecommendedProfiles = `
+query RecommendedProfiles {
+    recommendedProfiles {
+          id
+        name
+        bio
+        attributes {
+          displayType
+          traitType
+          key
+          value
+        }
+          followNftAddress
+        metadata
+        isDefault
+        picture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        handle
+        coverPicture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        ownedBy
+        dispatcher {
+          address
+          canUseRelay
+        }
+        stats {
+          totalFollowers
+          totalFollowing
+          totalPosts
+          totalComments
+          totalMirrors
+          totalPublications
+          totalCollects
+        }
+        followModule {
+          ... on FeeFollowModuleSettings {
+            type
+            amount {
+              asset {
+                symbol
+                name
+                decimals
+                address
+              }
+              value
+            }
+            recipient
+          }
+          ... on ProfileFollowModuleSettings {
+           type
+          }
+          ... on RevertFollowModuleSettings {
+           type
+          }
+        }
+    }
+  }`
