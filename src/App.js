808e578efedf210d32ec85d0ae20c11523ad24c4
yusuffikri <yusuffiks99@gmail.com>
2023-02-28 22:12:32 +0800

    lens hub update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { urlClient, LENS_HUB_CONTRACT_ADRESS, queryRecommendedProfiles, queryExplorePublications} from './queries'
+import LENSHUB from "./lenshub.json"
+import { ethers } from "ethers";
+import {Box, Button, Image} from "@/chakra-ui/react"
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [profiles, setProfiles] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+
   return (
     <div className="app">
 
