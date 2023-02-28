a7a44037b71f5598bbb763b562d9906abd1a52ed
yusuffikri <yusuffiks99@gmail.com>
2023-02-28 22:21:18 +0800

    app.js update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import { ethers } from "ethers";
 import {Box, Button, Image} from "@/chakra-ui/react"
 
 function App() {
-  const [account, setAccount] = useState(null);
+  const [accounts, setAccounts] = useState(null);
   const [profiles, setProfiles] = useState([]);
   const [posts, setPosts] = useState([]);
+
+  async function signIn(){
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
+    setAccounts(accounts[0]);
+  }
+  async function getRecommendedProfiles(){
+    const response = await urlClient.query(queryRecommendedProfiles).toPromise();
+    const profiles = response.data.RecommendedProfiles.slice(0.5);
+  }
 
 
   return (
